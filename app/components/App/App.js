95652f5722ccf9c97fe20f19521c96f13b0d041e
Fritz Jay <jonathan.jay10@gmail.com>
2018-10-30 13:34:11 -0700

    Added Header and inputs with some styling

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './App.css'
 
+import { Header } from '../Header/Header'
+
 export const App = () => {
   return (
-    <div className="app" style={{ backgroundImage: "url('app/images/background.svg')"}}>
-      Hello World
+    <div className="app-component" style={{ backgroundImage: "url('app/images/background.svg')"}}>
+
+      <Header>
+        <h1>Clever Title</h1>
+      </Header>
+      
+      <div className="city-state-container">
+      
+        <h1 className="city-state-header">
+          Enter a City and State
+        </h1>
+
+        <input
+          className="city-state-input"
+          placeholder="St. George, Utah" />
+
+        <button className="city-state-button">
+          Get Weather
+        </button>
+      
+      </div>
+
     </div>
   )
 }
