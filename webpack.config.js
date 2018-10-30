597d4072c2eecbbad8c77ea747885bbb13547c75
Fritz Jay <jonathan.jay10@gmail.com>
2018-10-30 15:18:18 -0700

    added dotenv and environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 var path = require('path')
+var dotenv = require('dotenv')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// call dotenv and it will return an Object with a parsed key 
+const env = dotenv.config().parsed;
+  
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   entry: './app/index.js',
@@ -18,6 +29,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/index.html'
-    })
+    }),
+    new webpack.DefinePlugin(envKeys),
   ]
 }
