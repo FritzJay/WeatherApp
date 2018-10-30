befb02ed5b2139b24a47e38c283deef243abdef9
Fritz Jay <jonathan.jay10@gmail.com>
2018-10-30 13:34:26 -0700

    Updated header to take children and added styling.

---
+++ app/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = ({ children }) => {
+  return (
+    <div className="header-component">
+      {children
+        ? children
+        : null}
+    </div>
+  )
+}
